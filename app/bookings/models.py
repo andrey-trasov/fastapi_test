60b6d1b56b5d7494a025ae33b6a1221b4c980642
andrey-trasov <159887936+andrey-trasov@users.noreply.github.com>
2025-02-20 18:05:42 +0300

    123

--- app/bookings/models.py
+++ app/bookings/models.py
@@ -7,7 +7,7 @@ class Bookings(Base):
     __tablename__ = "bookings"
     id = Column(Integer, primary_key=True)
     room_id = Column(ForeignKey("rooms.id"), nullable=False)
-    user_id = Column(ForeignKey("rooms.id"), nullable=False)
+    user_id = Column(ForeignKey("users.id"), nullable=False)
     date_from = Column(Date, nullable=False)
     date_to = Column(Date, nullable=False)
     price = Column(Integer, nullable=False)
